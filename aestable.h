098aa0dd6539581e982a418bcef341fb543c7124
Yorick de Wid <yorick17@outlook.com>
2014-06-02 12:08:40 +0200

    add shared variables

---
+++ aestable.h
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <memory.h>
+
+typedef unsigned char uchar;
+
+#define Nb 4			// number of columns in the state & expanded key
+#define Nk 4			// number of columns in a key
+#define Nr 10			// number of rounds in encryption
+
+void ShiftRows(uchar *);
+void InvShiftRows(uchar *);
+void MixSubColumns(uchar *);
+void InvMixSubColumns(uchar *);
+void AddRoundKey(unsigned *, unsigned *);
+void ExpandKey(uchar *, uchar *);
+void Encrypt(uchar *, uchar *, uchar *);
+void Decrypt(uchar *, uchar *, uchar *);
